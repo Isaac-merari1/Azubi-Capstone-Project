0862d8d3cf1450059b68aa08207cc4570850c22d
Isaac-merari1 <52751488+Isaac-merari1@users.noreply.github.com>
2022-05-18 09:14:20 +0000

    Add files via upload
    Capstone project for looping and working with numbers

---
+++ loopingAndSkipping.sh
@@ -0,0 +1,4 @@
+
+#https://github.com/Isaac-merari1/Azubi-Capstone-Project.git
+
+seq 1 2 99
---
+++ workingWithNumbers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#https://github.com/Isaac-merari1/Azubi-Capstone-Project.git
+read x
+read y
+
+echo $((x+y))
+echo $((x-y))
+echo $((x*y))
+echo $((x/y))
+
